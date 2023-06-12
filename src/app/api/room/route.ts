26ac4c1ad412ff481428e2c9f38c5beafe6949e3
AlfredHutomo <alfredhutomo@gmail.com>
2023-06-12 23:11:04 +0700

    added logic to get chat room based on userId

--- src/app/api/room/route.ts
+++ src/app/api/room/route.ts
@@ -1,5 +1,35 @@
+import { Room } from "@prisma/client";
 import { NextResponse } from "next/server";
 import { prisma } from "~/utils/prisma";
+
+// Create chat room
+export async function GET(request: Request) {
+  const url = new URL(request.url);
+  const userId = url.searchParams.get("userId");
+  const userType = url.searchParams.get("userType");
+
+  if (!userId) {
+    return NextResponse.json({ error: "Missing userId" });
+  }
+
+  let userRooms: Room[] = [];
+
+  if (userType === "DOCTOR") {
+    userRooms = await prisma.room.findMany({
+      where: {
+        doctorId: userId,
+      },
+    });
+  } else {
+    userRooms = await prisma.room.findMany({
+      where: {
+        userId: userId,
+      },
+    });
+  }
+
+  return NextResponse.json(userRooms);
+}
 
 // Create chat room
 export async function POST(request: Request) {
